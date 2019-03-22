04335d6fcc22794aa16cb24bdbbef0b450f750e0
Akito SUN <akitosun@outlook.com>
2019-03-22 14:01:04 +0800

    [master]Init

---
+++ WPF loggin/WPF loggin/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_loggin
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Properties["UserName"] = UserNameTextBox.Text;
+            var main = new MainWindow();
+            this.Close();
+            main.Show();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
