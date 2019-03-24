b53f14bb685b61a7bf2f717f8aef5c1a13512c98
Akito SUN <akitosun@outlook.com>
2019-03-24 22:01:44 +0800

    [master]實作密碼驗證、DAL

---
+++ WPF loggin/DB/dbo/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           CHAR (36)      NOT NULL,
+    [UserName]     NVARCHAR(50)   NOT NULL,
+    [PasswordHash] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
--- WPF loggin/WPF loggin/Login.xaml.cs
+++ WPF loggin/WPF loggin/Login.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,14 +12,17 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DAL;
 
 namespace WPF_loggin
 {
+
     /// <summary>
     /// Interaction logic for Login.xaml
     /// </summary>
     public partial class Login : Window
     {
+        private WPFDBEntities db = new WPFDBEntities();
         public Login()
         {
             InitializeComponent();
@@ -26,15 +30,50 @@ namespace WPF_loggin
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            App.Current.Properties["UserName"] = UserNameTextBox.Text;
-            var main = new MainWindow();
-            this.Close();
-            main.Show();
+            if (LoginCheck())
+            {
+                App.Current.Properties["UserName"] = UserNameInput.Text;
+                var main = new MainWindow();
+                this.Close();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("登入失敗");
+            }
         }
 
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
+            var result = HashPassword();
+            var dto = new User();
+            dto.UserName = UserNameInput.Text;
+            dto.PasswordHash = result;
+            dto.Id = Guid.NewGuid().ToString();
+            db.Users.Add(dto);
+            db.SaveChanges();
+            MessageBox.Show("註冊成功，請重新登入!");
+            UserNameInput.Text = string.Empty;
+            PasswordInput.Password = string.Empty;
+        }
 
+        private bool LoginCheck()
+        {
+            var dto = db.Users.FirstOrDefault(x => x.UserName == UserNameInput.Text);
+            if (dto == null) return false;
+            var result = HashPassword();
+            if (dto.PasswordHash != result) return false;
+            return true;
+        }
+
+        private string HashPassword()
+        {
+            SHA512 sha512 = new SHA512CryptoServiceProvider(); //建立一個SHA512
+            var salt = UserNameInput.Text;
+            var password = Encoding.Default.GetBytes(salt + PasswordInput.Password); //將字串轉為Byte[]
+            var crypto = sha512.ComputeHash(password); //進行SHA512加密
+            var result = Convert.ToBase64String(crypto); //把加密後的字串從Byte[]轉為字串
+            return result;
         }
     }
 }
